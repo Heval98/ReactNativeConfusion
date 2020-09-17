7f936ced999ec34baaef6920f8cf7ea951527af5
Heval98 <jmheval@gmail.com>
2020-09-17 17:27:46 -0500

    Ract Native Navigation Part 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,31 +1,84 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
 import Dishdetail from './DishdetailComponent';
-import { View } from 'react-native';
+import { View, Platform, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// const MenuNavigator = createStackNavigator({
+//     Menu: { screen: Menu },
+//     Dishdetail: { screen: Dishdetail}
+// }, {
+//     initialRouteName: 'Menu',
+//     navigationOptions: {
+//         headerStyle: {
+//             backgroundColor: '#512DA8'
+//         },
+//         headerTintColor: '#fff',
+//         headerTitleStyle: {
+//             color: '#fff'
+//         }
+//     }
+// });
+
+// const Stack = createStackNavigator();
+
+// function MyStack() {
+//     return (
+//         <Stack.Navigator initialRouteName='Menu'
+//             screenOptions={{
+//                             headerStyle: {
+//                                 backgroundColor: "#512DA8"
+//                             },
+//                             headerTintColor: "#fff",
+//                             headerTitleStyle: {
+//                                 color: "#fff"            
+//                             }
+//                         }}
+//         >
+//           <Stack.Screen name="Menu" component={Menu} />
+//           <Stack.Screen name="Dishdetail" component={Dishdetail} options={{ headerTitle: "Dish Detail"}} />
+//         </Stack.Navigator> 
+//       );
+// }
+
+const MenuNavigator = createStackNavigator();
+
+function MenuNavigatorScreen({ navigation }) {
+    return(
+        <MenuNavigator.Navigator
+            initialRouteName='Menu'
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: "#512DA8"
+                },
+                headerTintColor: "#fff",
+                headerTitleStyle: {
+                    color: "#fff"            
+                }
+            }}
+        >
+            <MenuNavigator.Screen
+                name="Menu"
+                component={Menu}
+            />
+            <MenuNavigator.Screen
+                name="Dishdetail"
+                component={Dishdetail}
+                options={{ headerTitle: "Dish Detail"}}
+            />            
+        </MenuNavigator.Navigator>
+    );
+}
 
 class Main extends Component {
 
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            dishes: DISHES,
-            selectedDish: null
-        }
-    }
-
-    onDishSelect(dishId) {
-        this.setState({selectedDish: dishId});
-    }
-
     render() {
         return (
-            <View>
-                <Menu dishes={this.state.dishes}
-                    onPress={(dishId) => this.onDishSelect(dishId)}
-                />
-                <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+            <View style={{flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight}}>
+                <NavigationContainer>
+                    <MenuNavigatorScreen />
+                </NavigationContainer>
             </View>
         );
     }
