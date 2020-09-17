dbbf0c677f100b93b7f75c5cf5b0ec9499ce382d
Heval98 <jmheval@gmail.com>
2020-09-17 11:13:58 -0500

    React Native Components Part 2

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import GlobalStyles from './shared/GlobalStyles';
 import React from 'react';
 import Main from './components/MainComponent';
+import { View } from 'react-native';
 
 export default function App() {
   return (
-    <Main style={GlobalStyles.droidSafeArea} />
+    <View style={GlobalStyles.droidSafeArea}>
+      <Main />
+    </View>
   );
 }
 
