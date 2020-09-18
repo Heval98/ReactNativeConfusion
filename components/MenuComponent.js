7b748c59f9311543ecc6ca942991670817de3942
Heval98 <jmheval@gmail.com>
2020-09-17 20:30:42 -0500

    Prueba ListItem

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import { View, FlatList } from 'react-native';
-import { ListItem } from 'react-native-elements';
 import { DISHES } from '../shared/dishes';
+import { View, FlatList, Text } from 'react-native';
+import { ListItem } from 'react-native-elements';
 
 class Menu extends Component {
 
-    state = {
-        dishes: DISHES
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
     }
     
     static navigationOptions = {
@@ -19,16 +22,19 @@ class Menu extends Component {
             return(
                 <ListItem
                     key={index}
-                    title={item.name}
-                    subtitle={item.description}
                     hideChevron={true}
                     onPress={() => navigate('Dishdetail', { dishId: item.id })}
-                    leftAvatar={{ source: require('./images/uthappizza.png') }}
-                />
+                    //Avatar={{ source: require('./images/uthappizza.png') }}
+                >
+                    <ListItem.Content>
+                    <ListItem.Title>{item.name}</ListItem.Title>
+                    <ListItem.Subtitle>{item.description}</ListItem.Subtitle>
+                    </ListItem.Content>
+                </ListItem>
             );
         }
 
-        const { navigate } = this.props.navigation;
+        // const { navigate } = this.props.navigation.navigate;
 
         return (
             <FlatList 
