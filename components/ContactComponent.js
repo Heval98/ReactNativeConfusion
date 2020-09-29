4db939f6a9772f56c4f6613c5b14b0803d6c8947
Heval98 <jmheval@gmail.com>
2020-09-29 09:24:48 -0500

    Email Sharing

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,37 +1,57 @@
 import React, { Component } from 'react';
 import { SafeAreaView, View, Text } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card , Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
 import { ScrollView } from 'react-native-gesture-handler';
+import * as MailComposer from 'expo-mail-composer';
 
-export default function Contact({ navigation }) {
 
-    return(
-        <ScrollView style={{marginTop: 25}}>
-            <Animatable.View animation="fadeInDown" duration={2000} delay={1000} >
-                <Card>
-                    <Card.Title>Contact Information</Card.Title>
-                    <Card.Divider />
-                        <Text style={{margin: 10}}>
-                            121, Clear Water Bay Road
-                        </Text>
-                        <Text style={{margin: 10}}>
-                            Clear Water Bay, Kowloon
-                        </Text>
-                        <Text style={{margin: 10}}>
-                            HONG KONG
-                        </Text>
-                        <Text style={{margin: 10}}>
-                            Tel: +852 1234 5678
-                        </Text>
-                        <Text style={{margin: 10}}>
-                            Fax: +852 8765 4321
-                        </Text>
-                        <Text style={{margin: 10}}>
-                            Email:confusion@food.net
-                        </Text>       
-                </Card>
-            </Animatable.View>
-        </ScrollView>
-    );
+ class Contact extends Component{
+
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['confusion@food.net'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern:'
+        })
+    }
+    
+    render(){
+        return(
+            <ScrollView style={{marginTop: 25}}>
+                <Animatable.View animation="fadeInDown" duration={2000} delay={1000} >
+                    <Card>
+                        <Card.Title>Contact Information</Card.Title>
+                        <Card.Divider />
+                            <Text style={{margin: 10}}>
+                                121, Clear Water Bay Road
+                            </Text>
+                            <Text style={{margin: 10}}>
+                                Clear Water Bay, Kowloon
+                            </Text>
+                            <Text style={{margin: 10}}>
+                                HONG KONG
+                            </Text>
+                            <Text style={{margin: 10}}>
+                                Tel: +852 1234 5678
+                            </Text>
+                            <Text style={{margin: 10}}>
+                                Fax: +852 8765 4321
+                            </Text>
+                            <Text style={{margin: 10}}>
+                                Email:confusion@food.net
+                            </Text>      
+                            <Button
+                                title="Send Email"
+                                buttonStyle={{backgroundColor: "#512DA8"}}
+                                icon={<Icon name='envelope-o' type='font-awesome' color='white' />}
+                                onPress={this.sendMail}
+                                />
+                    </Card>
+                </Animatable.View>
+            </ScrollView>
+        );
+    }
 }
+
+export default Contact;
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://192.168.0.5:3001/'
+export const baseUrl = 'http://192.168.0.20:3001/'
